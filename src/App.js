12571ac9e707da62e71933e9b94f4a4b4dddf55f
luo <luo@qq.com>
2019-06-09 23:21:19 +0800

    第二次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import './App.css'
+import './iconfont.css'
+import {NavBar} from 'antd-mobile';
+import {BrowserRouter as Router,Link,Switch,Route,Redirect} from 'react-router-dom'
+import {routes,RouteWithSubRoute} from './router'
+const homeNavRight = <Link to='/login'><i className='iconfont icon-denglu-copy'></i></Link>
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      leftContent:'网易彩票',
+      rightContent:homeNavRight
+    }
+  }
+  changeNav=(item)=>{
+    if(item){
+      this.setState({
+        leftContent:item.leftContent,
+        rightContent:''
+      })
+    }else{
+      let leftContent = '网易彩票'
+      this.setState({
+        leftContent:leftContent,
+        rightContent:homeNavRight
+      })
+    }
+  }
+  render(){
+    return(
+      <Router>
+      <div>
+        <NavBar
+          leftContent={this.state.leftContent}
+          rightContent={this.state.rightContent}
+        ></NavBar>
+        <Switch>
+          {
+            routes.map((item,index)=>{
+              return <RouteWithSubRoute key={item.name} {...item} changeNav={this.changeNav}/>
+            })
+          }
+          <Route path='/' render={()=>{
+            return <Redirect to="/home"></Redirect>
+          }}></Route>
+        </Switch>
+      </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,42 @@
+import React,{Component} from 'react'
+import {Button,Icon} from 'antd-mobile'
+import {Switch} from 'react-router-dom'
+import {RouteWithSubRoute} from '../../router'
+import './Login.css'
+
+class Login extends Component{
+    goBack=()=>{
+        this.props.history.go(-1)
+        this.props.changeNav()
+    }
+    componentDidMount(){
+        const leftContent = <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}
+                                onClick={this.goBack}    
+                                >
+                            <Icon type='left' size='lg'></Icon>返回</div>
+        this.props.changeNav({leftContent})
+    }
+    pushView=()=>{
+        let path = this.props.children[0].path
+        this.props.history.push({pathname:path})
+    }
+    render(){
+        return(
+            <div className='login'>
+                <input type='text' placeholder=' 网易邮箱/账号' className='userInfo'/>
+                <input type='text' placeholder=' 密码' className='userInfo'/>
+                <button className='loginBTN'>登 录</button>
+                <div className='loginBottom'>
+                    <Button style={{flex:1,height:'52px'}}>忘记密码?</Button>    
+                    <Button style={{flex:1,height:'52px'}} onClick={this.pushView}>注册</Button>
+                </div>
+                <Switch>
+                    {this.props.children.map((item,index)=>{
+                        return <RouteWithSubRoute key={item.name} {...item} changeNav={this.props.changeNav}></RouteWithSubRoute>
+                    })}
+                </Switch>
+            </div>
+        )
+    }
+}
+export default Login
